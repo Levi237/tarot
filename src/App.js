b69bb794dc36b6b773c3353d2b3ca7c1d25bd38e
Levi Winans <levieiko@gmail.com>
2022-11-01 16:36:57 -0700

    add component to app for preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import FetchData from './fetch';
+import FetchData from './tests/fetch';
+import StateTest from './tests/state';
 
 const App = () => {
   return (
     <div className="App">
-<FetchData/>
+      <FetchData/>
+      <StateTest/>
     </div>
   );
 }
