9d46b17f9f56c74c0004188e0835d03a934c6c5d
Levi Winans <levieiko@gmail.com>
2022-11-07 13:46:38 -0800

    add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { auth } from './firebase/config';
+import { onAuthStateChanged } from "firebase/auth";
 
 // import FetchData from './tests/fetch';
 // import StateTest from './tests/state';
-import AuthTest from './tests/auth/authenticate';
+import AuthTest from './tests/auth/';
 import Home from './pages/Home'
 const App = () => {
+
+const [user, userState] = useState([]);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      console.log("user.uid =>", user.uid);
+      userState(user);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+
   return (
     <div className="App">
+      { user ? <div>{user.displayName ? user.displayName : user.email}</div> : <div>Login</div>}
       {/* <FetchData/> */}
       {/* <StateTest/> */}
-       <AuthTest/>
       <Home />
+      <AuthTest user={user}/>
     </div>
   );
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
     const firebaseConfig = {
-        apiKey: "AIzaSyDGqIRtuLUpdpME4SSOIM2EE8id5K_HGEY",
+        apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
         authDomain: "tarot-app-2022.firebaseapp.com",
         databaseURL: "https://tarot-app-2022-default-rtdb.firebaseio.com",
         projectId: "tarot-app-2022",
         storageBucket: "tarot-app-2022.appspot.com",
         messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGER_ID}`,
-        appId: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+        appId: `${process.env.REACT_APP_FIREBASE_API_ID}`,
         measurementId: "G-J7QY6BWRTV"
     };
 
