da8c596f519813f25314fc2ee0ecf5cc96fb58cf
Levi Winans <levieiko@gmail.com>
2022-11-09 10:37:47 -0800

    start admin page with deck updates component

---
+++ src/pages/Admin.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Deck from '../tests/deck';
+
+const AdminPage = () => {
+
+   
+
+    return (
+        <>
+        <br/><br/><br/><br/><br/>
+Admin Page Test<br/>
+Logged In<br/>
+<Deck/>
+        </>
+    )
+}
+
+
+
+export default AdminPage;
