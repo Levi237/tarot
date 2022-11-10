3ac91bec32ed61865f9b8fcef68ba0e807a30565
Levi Winans <levieiko@gmail.com>
2022-11-10 11:58:42 -0800

    add onclick function

--- src/components/deck/index.js
+++ src/components/deck/index.js
@@ -5,11 +5,13 @@ import './card.css';
 
 const Deck = ({deck}) => {
 
-
+const doThis = (e, title) => {
+    console.log(title, "clicked card 'doThis'", e.currentTarget);
+}
         const mapDeck = deck.map((card, key) => {
             const img = card.title.toLowerCase().split(' ').join('_');
             return (
-                <div className="card" key={key}>
+                <div className="card" key={key} onClick={(e) => {doThis(e, card.title);}}>
                     <img src={`/cards/${img}.jpg`} alt={card.title}/>
                 </div>
             );
