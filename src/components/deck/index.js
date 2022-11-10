a2a078ddb4ace1d6271132d9641893afc32ca956
Levi Winans <levieiko@gmail.com>
2022-11-10 12:26:43 -0800

    create deck style, add scroll elements

--- src/components/deck/index.js
+++ src/components/deck/index.js
@@ -2,27 +2,31 @@
 import React, { useState, useEffect }   from 'react';
 
 import './card.css';
+import './deck.css';
 
 const Deck = ({deck}) => {
 
-const doThis = (e, title) => {
-    console.log(title, "clicked card 'doThis'", e.currentTarget);
-}
-        const mapDeck = deck.map((card, key) => {
-            const img = card.title.toLowerCase().split(' ').join('_');
-            return (
-                <div className="card" key={key} onClick={(e) => {doThis(e, card.title);}}>
-                    <img src={`/cards/${img}.jpg`} alt={card.title}/>
-                </div>
-            );
-        });
+    const doThis = (e, title) => {
+        console.log(title, "clicked card 'doThis'", e.currentTarget);
+    };
+    const mapDeck = deck.map((card, key) => {
+        const img = card.title.toLowerCase().split(' ').join('_');
+        return (
+            <div className="card" key={key} onClick={(e) => {doThis(e, card.title);}}>
+                <img src={`/cards/${img}.jpg`} alt={card.title}/>
+            </div>
+        );
+    });
 
-   
     return(
-        <>
-        {mapDeck}
+        <div className="deck-container">
+            <div class="deck">
+                <div className="cards">
+                    {mapDeck}
+                </div>
+            </div>
         <button>shuffle deck</button>
-        </>
+        </div>
     );
 };
 export default Deck;
