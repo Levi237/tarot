d9ba53285d4612aee50e3a5df1e03a6230bf3ace
Levi Winans <levieiko@gmail.com>
2022-11-10 09:51:58 -0800

    clean up component

--- src/components/nav/hamburger.js
+++ src/components/nav/hamburger.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+
 import './hamburger.css';
 
 const Hamburger = ({toggleNavMenu}) => {
 
-    return(<>
+    return(
         <div id="hamburger-container" name="toggleMenu" onClick={(e) => {toggleNavMenu(e); }}>
-     <nav id="hamburger-btn" className="">
-	<section></section>
-	<section></section>	
-     </nav>
- </div>
- </>
+            <nav id="hamburger-btn" className="">
+                <section></section>
+                <section></section>	
+            </nav>
+        </div>
     );
 };
 
