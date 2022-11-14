967227b6839d48b97ce2c667736592aa6aea6aa2
Levi Winans <levieiko@gmail.com>
2022-11-14 12:33:57 -0800

    wrap each section

--- src/components/modals/Card.js
+++ src/components/modals/Card.js
@@ -1,16 +1,24 @@
+//==> Create Card Modal to display all card information.  
+//========> Allow scroll to view all information
+
 import React from 'react';
 
-import './card.css'
+import './modal.css'
 const CardModal = ({card, closeCardModal}) => {
     const img = card.title.toLowerCase().split(' ').join('_');
     return(
-      <div className="cardmodal"><button onClick={closeCardModal}>X</button>
-      <div>
-          Card Modal {card.title}
-      </div>
-      <div>
-      <img style={{transform: `rotate(${180*card.rotation + 'deg'})`}} src={`/cards/${img}.jpg`} alt={`${card.title}`}/>
-      </div>
+      <div className="cardmodal modal">
+        <div className="close-modal-box">
+          <button className="close-modal-btn" onClick={closeCardModal}>X</button>
+        </div>
+        <div className="two-column-grid">
+          <section>
+            <img style={{transform: `rotate(${180*card.rotation + 'deg'})`}} src={`/cards/${img}.jpg`} alt={`${card.title}`}/>
+          </section>
+          <section>
+              Card Modal {card.title}
+          </section>
+        </div>
       </div>  
     );
 };
