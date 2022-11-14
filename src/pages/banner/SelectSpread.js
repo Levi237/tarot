156be9653f2c3eff477ebffaba77f6741bc29551
Levi Winans <levieiko@gmail.com>
2022-11-13 23:14:25 -0800

    move spread oprions banner to own component

--- src/pages/banner/SelectSpread.js
+++ src/pages/banner/SelectSpread.js
@@ -1,22 +1,16 @@
 import React, {useState}  from 'react';
-// import MenuList from '../components/menulist/MenuList';
-import SelectSpreadBanner from './banner/SelectSpread';
+import MenuList from '../../components/menulist/MenuList';
 
-// import './spread-banner.css';
+import './spread-banner.css';
 
-const HomePage = ({layout, layouts, selectLayout}) => {
+const SelectSpreadBanner = ({layout, layouts, selectLayout}) => {
 
-//    const [keyNum, setKeyNum] = useState(3)
+   const [keyNum, setKeyNum] = useState(3)
 
     return (
-        <div>
-            Home Page Test
-            <SelectSpreadBanner 
-                                    layouts={layouts} 
-                                    layout={layout}
-                                    selectLayout={selectLayout} />
-            {/* <div className="grid two-column-spread-preview">
-                <div>
+        <div className="two-column-spread-preview-wrapper">
+            <div className="grid two-column-spread-preview">
+                <div className="info-window">
                     <h2>{layout.name ? layout.name : layouts[keyNum].name}</h2>
                     <p>{layout.note ? layout.note : layouts[keyNum].note}</p>
                     <div className="spread-preview">
@@ -31,11 +25,11 @@ const HomePage = ({layout, layouts, selectLayout}) => {
                         defaultKeyNum={keyNum}
                     />
                 </div>
-            </div> */}
-        </div>
+            </div>
+            </div>
     )
 }
 
 
 
-export default HomePage;
+export default SelectSpreadBanner;
