d8f91cde4daf70b74c0445570b3d50001322e24d
Levi Winans <levieiko@gmail.com>
2022-11-13 21:53:18 -0800

    set default selection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import MenuList from '../components/menulist/MenuList';
 
 const HomePage = ({layout, layouts, selectLayout}) => {
 
-   
+   const [keyNum, setKeyNum] = useState(3)
 
     return (
         <>
-Home Page Test
-<div className="spread-preview">
-        <img src={`./spread/${layout.type ? layout.type : layouts[1].type}.png`}/>
-</div>
+            Home Page Test
+            <div className="spread-preview">
+                    <img src={`./spread/${layout.type ? layout.type : layouts[keyNum].type}.png`}/>
+            </div>
             <MenuList                 
                 list={layouts} 
                 selectedData={layout}
                 selectFunction={selectLayout} 
+                defaultKeyNum={keyNum}
             />
         </>
     )
