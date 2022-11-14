d9a78b6c4475657f26911d3a63f8339ae3d5d946
Levi Winans <levieiko@gmail.com>
2022-11-14 01:15:17 -0800

    add card image to modal

--- src/components/modals/Card.js
+++ src/components/modals/Card.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 import './card.css'
 const CardModal = ({card, closeCardModal}) => {
+    const img = card.title.toLowerCase().split(' ').join('_');
     return(
-      <div className="cardmodal"><button onClick={closeCardModal}>X</button>Card Modal {card.title}</div>  
+      <div className="cardmodal"><button onClick={closeCardModal}>X</button>
+      <div>
+          Card Modal {card.title}
+      </div>
+      <div>
+      <img style={{transform: `rotate(${180*card.rotation + 'deg'})`}} src={`/cards/${img}.jpg`} alt={`${card.title}`}/>
+      </div>
+      </div>  
     );
 };
 
