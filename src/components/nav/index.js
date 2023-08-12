d53deafb3a6940de96e041c0d42ba9ba848a1b61
Levi Winans <levieiko@gmail.com>
2023-08-12 16:22:00 -0700

    hide login

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -19,8 +19,8 @@ return (<>
         <div>
             <NavLink exact to={routes.ROOT} onClick={toggleNavMenu}>HOME</NavLink>
             <NavLink to={routes.DRAW} onClick={toggleNavMenu}>DRAW CARDS</NavLink>
-            { user.uid && <NavLink to={routes.ACCT} onClick={toggleNavMenu} >ACCOUNT</NavLink> }
-            { user.uid ? <button className="signout-" onClick={()=>{clickSignOut();toggleNavMenu();}}>Sign Out</button> : <button id="login-btn"  onClick={()=>{openSignIn();toggleNavMenu();}} >Login</button> }
+            {/* { user.uid && <NavLink to={routes.ACCT} onClick={toggleNavMenu} >ACCOUNT</NavLink> } */}
+            {/* { user.uid ? <button className="signout-" onClick={()=>{clickSignOut();toggleNavMenu();}}>Sign Out</button> : <button id="login-btn"  onClick={()=>{openSignIn();toggleNavMenu();}} >Login</button> } */}
         </div>
     </div></>)
 }
