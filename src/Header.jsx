6a5f67baf567ce3f5ddcd682cf94a5e5a8666943
Levi Winans <levieiko@gmail.com>
2023-10-30 11:26:24 -0700

    adjust header for mobile

--- src/Header.jsx
+++ src/Header.jsx
@@ -32,26 +32,3 @@ const Header = ({ user, clickSignOut, openSignIn, layout, layouts, selectLayout 
     );
 };
 export default Header;
-
-                // { layout.id
-                // ? <h4>{layout.name}</h4>
-                // : <Dropdown 
-                //     list={layouts} 
-                //     defaultText={"Choose a Spread"}
-                //     defaultValue={""}
-                //     selectedData={layout}
-                //     selectFunction={selectLayout} 
-                //     defaultOption={true}
-                //     />
-                // }
-                // </section>
-                // <section>
-                //     <button id="shuffle-btn" onClick={shuffleBtn}>shuffle deck</button>
-                // </section>
-                // <section>
-                //     <HamMenu>
-                //         <button id="reverse-btn" onClick={toggleUprightOnly} className="toggle-reverse-btn invisible-btn---- small btn">Upright Cards</button>
-                //         <button onClick={refreshDeck} className="invisible-btn---- small btn">REFRESH</button>
-                //         <button onClick={goBack} className="invisible-btn---- small btn">HOME</button>
-                //     </HamMenu>
-                // </section>
